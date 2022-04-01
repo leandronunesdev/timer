5f63ee25ce6419d0a590a4e44aa574c8280855ea
leandronunesdev <nunes.pessoal@gmail.com>
2022-04-01 19:16:59 -0300

    215. The Secret to the Animation

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  tick = () => {
-    console.log('tick');
-  };
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('timer started');
+  },
+  onTick() {
+    console.log('timer just ticked down');
+  },
+  onComplete() {
+    console.log('timer is completed');
+  },
+});
